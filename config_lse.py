09a7d748f280a2b303ab55d3172ba25361ddbd9d
zethier <zhou.ethier@philips.com>
2014-06-27 15:09:23 -0400

    adding chmod and chown automation.

--- config_lse.py
+++ config_lse.py
@@ -17,12 +17,31 @@ def create_path (path):
 		if exception.errno != errno.EEXIST:
 			raise
 
+def change_owner (path):
+	try:
+		command = 'chown -R eplaya:eplaya ' + path
+		os.popen("sudo -S %s"%(command), 'w').write('yourmama\n') 
+		print "change ownership on Directory: ", command
+	except OSError as exception:
+		print "Directory ", path, " already exist."
+		if exception.errno != errno.EEXIST:
+			raise
 
-#def change_mode (directory) :
+def change_mod (path):
+	try:
+		command = 'chmod -R a+wx ' + path
+		os.popen("sudo -S %s"%(command), 'w').write('yourmama\n') 
+		print "change permission on Directory: ", command
+	except OSError as exception:
+		print "Directory ", path, " already exist."
+		if exception.errno != errno.EEXIST:
+			raise
 
 create_path ('/usr/local/ePlaya');
 create_path ('/usr/local/ePlaya/bin');
 create_path ('/usr/local/ePlaya/lib');
 
-#command = 'mkdir /usr/local/ePlaya'
-#os.popen("sudo -S %s"%(command), 'w').write('yourmama\n')
+change_owner ('/usr/local/ePlaya');
+
+change_mod('/usr/local/ePlaya');
+
